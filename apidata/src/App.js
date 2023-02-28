5ab4e1475a868d1342d430a4ec3836b170c235b2
shivam <cvampatel02@gmail.com>
2023-02-28 11:36:37 +0530

    apidata

---
+++ apidata/src/App.js
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Getdata from './component/api';
+import Singlepost from "./component/Singlepost";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Getdata />}></Route>
+        <Route path='/view/:id' element={<Singlepost/>}></Route>
+      </Routes>
+      
+    </BrowserRouter>
+  )
+}
+
+export default App;
